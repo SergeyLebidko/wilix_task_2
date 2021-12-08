5f55df52a4e6edcb43e339e57ccc60843ec03e75
SergeyLebidko <it.sergeyler@mail.ru>
2021-12-08 15:49:30 +0300

    В Card использован useCallback

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, {useCallback} from "react";
 import PropTypes from "prop-types";
 import "./Card.scss";
 
 function Card({size, content}) {
-    const getCardInline = () => {
+    const getCardInline = useCallback(() => {
         const base = Math.floor(70 / size);
         return {
             width: `${base}vmin`,
             height: `${base}vmin`,
             fontSize: `${(base - 3) < 2 ? 2 : (base - 3)}vmin`
         };
-    }
+    }, [size]);
 
     return (
         <div className="card" style={getCardInline()}>
